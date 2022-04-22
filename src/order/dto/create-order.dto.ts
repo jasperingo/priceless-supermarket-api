e8c0a2f8e80f90f5b807cb1462ac817b9b030ff4
Jasper Anelechukwu <jasperanels@gmail.com>
2022-04-22 16:32:00 +0100

    Modified order validation

--- src/order/dto/create-order.dto.ts
+++ src/order/dto/create-order.dto.ts
@@ -1,28 +1,57 @@
 import { Expose, Type } from 'class-transformer';
-import { IsArray, IsNotEmpty, ValidateNested } from 'class-validator';
+import {
+  ArrayMinSize,
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  ValidateNested,
+} from 'class-validator';
+import { ErrorCode } from 'src/error/error-code.constants';
+import {
+  numberErrorMessage,
+  requiredErrorMessage,
+  validationErrorMessage,
+} from 'src/error/validation-error-message.function';
 
 export class CreateOrderItemDto {
   @Expose()
-  @IsNotEmpty()
-  quanity: number;
+  @IsNumber({}, numberErrorMessage())
+  @IsNotEmpty(requiredErrorMessage())
+  quantity: number;
 
   @Expose({ name: 'product_id' })
-  @IsNotEmpty()
+  @IsNumber({}, numberErrorMessage())
+  @IsNotEmpty(requiredErrorMessage('product_id'))
   productId: number;
 }
 
 export class CreateOrderDto {
   @Expose({ name: 'delivery_address_city' })
-  @IsNotEmpty()
+  @IsNotEmpty(requiredErrorMessage('delivery_address_city'))
   deliveryAddressCity: string;
 
   @Expose({ name: 'delivery_address_state' })
-  @IsNotEmpty()
+  @IsNotEmpty(requiredErrorMessage('delivery_address_state'))
   deliveryAddressState: string;
 
   @Type(() => CreateOrderItemDto)
   @Expose({ name: 'order_items' })
-  @ValidateNested()
-  @IsArray()
+  @ValidateNested({ each: true })
+  @ArrayMinSize(
+    1,
+    validationErrorMessage(
+      'errors.field_min_length',
+      ErrorCode.FIELD_MIN_LENGTH,
+      'order_items',
+    ),
+  )
+  @IsArray(
+    validationErrorMessage(
+      'errors.field_not_array',
+      ErrorCode.FIELD_NOT_ARRAY,
+      'order_items',
+    ),
+  )
+  @IsNotEmpty(requiredErrorMessage('order_items'))
   orderItems: CreateOrderItemDto[];
 }
