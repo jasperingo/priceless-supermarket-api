13c74f5be313108cedebea8e498db8ee407f18b5
Jasper Anelechukwu <jasperanels@gmail.com>
2022-05-04 13:08:44 +0100

    Added deleting old photo after update

--- src/photo/photo.service.ts
+++ src/photo/photo.service.ts
@@ -1,12 +1,17 @@
 import * as sizeOf from 'image-size';
+import { unlink } from 'fs';
 import { promisify } from 'util';
 import { Injectable } from '@nestjs/common';
 import { PhotoRepository } from './photo.repository';
 import { Photo } from './entities/photo.entity';
+import { PhotoLocationService } from './photo-location.service';
 
 @Injectable()
 export class PhotoService {
-  constructor(private readonly photoRepository: PhotoRepository) {}
+  constructor(
+    private readonly photoRepository: PhotoRepository,
+    private readonly photoLocationService: PhotoLocationService,
+  ) {}
 
   getDimensions(photo: Express.Multer.File) {
     return promisify(sizeOf.imageSize)(photo.path);
@@ -28,12 +33,15 @@ export class PhotoService {
   }
 
   async update(photo: Photo, uploaded: Express.Multer.File) {
+    const oldPath = this.photoLocationService.path(photo);
     const dimensions = await this.getDimensions(uploaded);
     photo.name = uploaded.filename;
     photo.mimeType = uploaded.mimetype;
     photo.size = uploaded.size;
     photo.width = dimensions.width;
     photo.height = dimensions.height;
-    return this.photoRepository.save(photo);
+    await this.photoRepository.save(photo);
+    await promisify(unlink)(oldPath);
+    return this.photoRepository.findOne(photo.id);
   }
 }
